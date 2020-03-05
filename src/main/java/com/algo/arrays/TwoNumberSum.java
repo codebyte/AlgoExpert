4e90c7ef5403be654be16f82879864435b95f179
codebyte <sujeet.jog@gmail.com>
2020-03-05 21:35:45 +0530

    create individual packages,

--- src/main/java/com/algo/arrays/TwoNumberSum.java
+++ src/main/java/com/algo/arrays/TwoNumberSum.java
@@ -1,11 +1,13 @@
+package com.algo.arrays;
+
 import java.util.Arrays;
 
 public class TwoNumberSum {
 
 
-    public static int[] toNumberSum(int[]a , int targetSum) {
+    public static int[] toNumberSum(int[] a, int targetSum) {
 
-        int pair[] = {0,0};
+        int pair[] = {0, 0};
 
         int left = 0;
         int right = 0;
@@ -14,9 +16,9 @@ public class TwoNumberSum {
 
         System.out.println("==> a[] " + Arrays.toString(a));
 
-        for(left = 0, right = a.length -1; (right >= 0)  && (left <= a.length -1 ); ) {
+        for (left = 0, right = a.length - 1; (right >= 0) && (left <= a.length - 1); ) {
 
-            if(left == right) {
+            if (left == right) {
                 break;
             }
             int sum = a[left] + a[right];
@@ -31,7 +33,7 @@ public class TwoNumberSum {
             }
         }
 
-        if((pair[0] == 0) && (pair[1] == 0)) {
+        if ((pair[0] == 0) && (pair[1] == 0)) {
             return null;
         } else {
             return pair;
@@ -42,7 +44,7 @@ public class TwoNumberSum {
 
         System.out.println("Hello World");
 
-        int a[] = {2,3,1,7,9,5};
+        int a[] = {2, 3, 1, 7, 9, 5};
 
         System.out.println(Arrays.toString(toNumberSum(a, 8)));
 
